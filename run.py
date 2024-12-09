d0665dd1b566aacfedafd99dabb1bb1f5e2746f3
Vansh Kashyap <145321719+Vansh-kash2023@users.noreply.github.com>
2024-12-09 16:51:54 +0530

    changes done for deployment

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from app import create_app
 from dotenv import load_dotenv
 
-load_dotenv()  # Automatically loads environment variables from .env file
+# Load environment variables from the .env file (if present)
+load_dotenv()
 
+# Create the Flask app instance
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)  # Local development
+# Only run the app with Flask's built-in server if this script is run directly
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)  # Use for local development
